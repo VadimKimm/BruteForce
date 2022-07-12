56e055c290cf779f2dd119979d29d6424a0a6a3e
Vadim <vadim_kimm@icloud.com>
2022-07-12 23:44:09 +0300

    Created enum Strings

---
+++ Pr2503/Sources/Extensions/BruteForceViewControllerExtension.swift
@@ -0,0 +1,14 @@
+//
+//  BruteForceViewControllerExtension.swift
+//  Pr2503
+//
+//  Created by Vadim Kim on 12.07.2022.
+//
+
+import UIKit
+
+extension BruteForceViewController {
+    enum Strings {
+        static let passwordLabelText = "Дай мне угадать пароль..."
+    }
+}
